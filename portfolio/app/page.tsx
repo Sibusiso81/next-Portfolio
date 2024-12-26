5bd52032e14bb60f9e3cb8253830e95ba494262d
Sibusiso81 <zulusibusiso81@gmail.com>
2024-12-27 00:22:11 +0200

    value add section

--- portfolio/app/page.tsx
+++ portfolio/app/page.tsx
@@ -83,7 +83,7 @@ export default function Home() {
             </p>
           </div>
         </div>
-        <div className="w-full h-[2px] bg-neutral-800  mt-40 md:mt-60 lg:mt-72 xl:mt-96"></div>
+        <div className="w-full h-[2px] bg-neutral-800  mt-20 md:mt-58 "></div>
         <div className="flex flex-col md:flex-row justify-between p-2">
           <p>Based on Johannesburg &#44;South Africa</p>
           <div className="flex flex-row space-x-1">
@@ -160,6 +160,73 @@ export default function Home() {
             </div>
           </div>
         </div>
+      </section>
+      <section className="w-screen h-screen  p-4 lg:p-20 font-mono  ">
+     <h1 className=" text-xl md:text-2xl lg:text-3xl font-bold space-y-6 pb-14 lg:pb-20"> How I Can Add Value to Your Team</h1>
+       <div className="grid grid-cols-4 gap-6 lg:gap-2">
+         {/*  */}
+       
+        <div className="col-span-4 md:col-span-2 lg:col-span-1 border-l-2 border-l-[#ebecf4] dark:border-l-[#aeaeae] flex flex-col pl-4">
+          {/* SVG / NUMBER */}
+          <div className="space-y-4 ">
+            <p className="text-green-400">01</p>
+          </div>
+          {/* Header */}
+          <div className="text-lg font-semibold py-2 lg:py-6">
+            <h1>Data-Driven HR Insights</h1>
+          </div>
+          {/* Description */}
+          <div className="dark:text-muted-foreground">
+            <p>Leveraging my tech skills, I can analyze HR data to identify trends, uncover valuable insights, and provide data-driven recommendations for improving HR processes.</p>
+          </div>
+        </div>
+            {/*  */}
+            <div className="col-span-4 md:col-span-2 lg:col-span-1 border-l-2 border-l-[#ebecf4] dark:border-l-[#aeaeae] flex flex-col pl-4">
+          {/* SVG / NUMBER */}
+          <div className="">
+            <p className="text-green-400">02</p>
+          </div>
+          {/* Header */}
+          <div className="text-lg font-semibold py-2 lg:py-6">
+            <h1>Driving Innovation in HR</h1>
+          </div>
+          {/* Description */}
+          <div className="dark:text-muted-foreground">
+            <p>I'm passionate about exploring and implementing innovative HR solutions, such as AI-powered recruitment tools or employee engagement platforms, to drive efficiency and improve outcomes.</p>
+          </div>
+        </div>
+            {/*  */}
+            <div className="col-span-4 md:col-span-2 lg:col-span-1 border-l-2 border-l-[#ebecf4] dark:border-l-[#aeaeae] flex flex-col pl-4">
+          {/* SVG / NUMBER */}
+          <div className="">
+            <p className="text-green-400">03</p>
+          </div>
+          {/* Header */}
+          <div className="text-lg font-semibold py-2 lg:py-6">
+            <h1>A Continuous Learner</h1>
+          </div>
+          {/* Description */}
+          <div className="dark:text-muted-foreground">
+            <p>As a self-taught developer, I'm a fast learner and highly adaptable. I'm eager to embrace new technologies and challenges, and I believe in continuous learning and professional growth.</p>
+          </div>
+        </div>
+            {/*  */}
+            <div className="col-span-4 md:col-span-2 lg:col-span-1 border-l-2 border-l-[#ebecf4] dark:border-l-[#aeaeae] flex flex-col pl-4">
+          {/* SVG / NUMBER */}
+          <div className="">
+            <p className="text-green-400">04</p>
+          </div>
+          {/* Header */}
+          <div className="text-lg font-semibold py-2 lg:py-6">
+            <h1>Human-Centered Technology</h1>
+          </div>
+          {/* Description */}
+          <div className="dark:text-muted-foreground">
+            <p>My HR background allows me to ensure a human-centered approach to all tech solutions, prioritizing user experience and creating tools that are both effective and enjoyable to use.</p>
+          </div>
+        </div>
+       </div>
+           
       </section>
     </main>
   );
